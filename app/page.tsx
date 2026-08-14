a2030154654b6d2081899e7f66b7b60a7fe647d6
Manuel Lari <manuelylari@gmail.com>
2026-08-14 16:10:47 +0200

    wer

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,14 @@ export default function Home() {
 
         {/* COS'È FANTAMICCIO */}
 
-        <section id="scopri" className="py-24 bg-white">
+        <section id="scopri" className="py-24 bg-[#F9F5EF]">
           <div className="max-w-5xl mx-auto">
 
             <h2 className="text-4xl font-bold text-center mb-8">
-              🏇 Cos'è FantaMiccio?
+              🫏 Cos'è FantaMiccio?
             </h2>
 
-            <p className="text-lg text-gray-700 leading-8">
+            <p className="text-lg text-[#5C3A21] leading-8">
               <strong>FantaMiccio è il fantasy game dedicato al Palio dei Micci di Querceta.</strong>
               <br /><br />
 
@@ -47,8 +47,8 @@ export default function Home() {
               Anche se in un certo evento non puoi essere presente, puoi sostenere la tua Contrada e vivere la stessa esperienza: scegli quella per cui fai il tifo, rappresentala nelle classifiche e contribuisci a renderla la più popolare.
             </p>
 
-            <div className="grid gap-6 mt-12 md:grid-cols-2">
-
+            <div className="text-[#5C3A21]">
+              
               <div>🛡️ <strong>Scegli la tua Contrada del cuore</strong></div>
 
               <div>⚽ <strong>Crea la squadra del Torneo di Calcio</strong></div>
@@ -57,7 +57,7 @@ export default function Home() {
 
               <div>🏆 <strong>Sfida amici e altri Fantacontradaioli</strong></div>
 
-              <div>📈 <strong>Fai crescere la popolarità della tua Contrada</strong></div>
+              <div>⭐ <strong>Fai crescere la popolarità della tua Contrada</strong></div>
 
               <div>👑 <strong>Diventa il miglior Fantacontradaiolo della stagione</strong></div>
 
