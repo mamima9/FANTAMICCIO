f1b6d44221a0829521af73440beac17cffe1665e
mamima9 <142883859+mamima9@users.noreply.github.com>
2026-09-25 21:25:38 +0200

    polish(godot): improve player movement animation

--- godot/scripts/Player.gd
+++ godot/scripts/Player.gd
@@ -4,17 +4,19 @@ extends CharacterBody2D
 var facing := Vector2.DOWN
 var moving := false
 var bob := 0.0
+var step_time := 0.0
 
 @onready var sprite: Sprite2D = $Sprite
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
     var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
     velocity = input * speed
     moving = input.length() > 0.05
 
     if moving:
         facing = input.normalized()
-        bob += _delta * 12.0
+        bob += delta * 12.0
+        step_time += delta
         sprite.position.y = sin(bob) * 2.0
         sprite.frame = int(Time.get_ticks_msec() / 130.0) % 4
     else:
